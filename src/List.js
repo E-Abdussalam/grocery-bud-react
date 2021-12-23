0fed161fdbcf856adf211e017c732f4667150c72
Emhemmed Abdussalam <emhemmed@Emhemmeds-iMac.local>
2021-12-23 00:11:52 +0000

    first commit

---
+++ src/List.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, removeItem, editItem }) => {
+  const handleEdit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button
+                type="submit"
+                className="edit-btn"
+                onClick={() => editItem(id)}
+              >
+                <FaEdit />
+              </button>
+              <button
+                type="submit"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
